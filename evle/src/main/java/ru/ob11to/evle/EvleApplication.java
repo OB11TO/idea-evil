7a3c7f4e6bddc4305839ac0c221c0d3cf6876b00
Artem Dagridjan <adagridjan@alvioneurope.ru>
2024-09-09 12:42:39 +0300

    first commit

---
+++ evle/src/main/java/ru/ob11to/evle/EvleApplication.java
@@ -0,0 +1,13 @@
+package ru.ob11to.evle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvleApplication.class, args);
+	}
+
+}
---
+++ evle/src/test/java/ru/ob11to/evle/EvleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ob11to.evle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
