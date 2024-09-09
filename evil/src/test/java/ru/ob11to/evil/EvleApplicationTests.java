37068eb4e5e46ebab395c25687c97954f8ed3a6b
Artem Dagridjan <adagridjan@alvioneurope.ru>
2024-09-09 13:05:50 +0300

    first commit

---
+++ evil/src/test/java/ru/ob11to/evil/EvleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ob11to.evil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
